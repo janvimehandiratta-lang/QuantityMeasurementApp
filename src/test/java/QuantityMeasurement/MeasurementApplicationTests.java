07f95e009956a575849d9ca44784ddaa9e9e028d
janvimehandiratta-lang <janvimehandiratta@gmail.com>
2026-03-25 15:34:37 +0530

    initial commit -main

---
+++ src/test/java/QuantityMeasurement/MeasurementApplicationTests.java
@@ -0,0 +1,13 @@
+package QuantityMeasurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasurementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
