d7b55d1dcdabe4042b38bb38a6df6e17af7485eb
janvimehandiratta-lang <janvimehandiratta@gmail.com>
2026-03-27 10:26:20 +0530

    code added

--- src/main/java/QuantityMeasurement/MeasurementApplication.java
+++ src/main/java/QuantityMeasurement/MeasurementApplication.java
@@ -1,13 +1,55 @@
 package QuantityMeasurement;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MeasurementApplication {
+	 static class Feet {
+	        private final double value;
+	        // Constructor
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeasurementApplication.class, args);
-	}
+	        public Feet(double value) {
+
+	            this.value = value;
+
+	        }
+	        // Override equals method
+
+	        @Override
+
+	        public boolean equals(Object obj) {
+
+	            if (this == obj) return true;
+
+
+
+	            if (obj == null || getClass() != obj.getClass())
+
+	                return false;
+
+
+
+	            Feet other = (Feet) obj;
+
+
+
+	            return Double.compare(this.value, other.value) == 0;
+
+	        }
+
+	    }
+
+	    // Main method to test
+
+	    public static void main(String[] args) {
+
+	    	//SpringApplication.run(MeasurementApplication.class,args);
+
+	        Feet f1 = new Feet(5.0);
+
+	        Feet f2 = new Feet(5.0);
+	        System.out.println("Are equal? " + f1.equals(f2));
+
+	    }
 
 }
--- src/test/java/QuantityMeasurement/MeasurementApplicationTests.java
+++ src/test/java/QuantityMeasurement/MeasurementApplicationTests.java
@@ -1,13 +1,36 @@
 package QuantityMeasurement;
-
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class MeasurementApplicationTests {
-
 	@Test
-	void contextLoads() {
-	}
+    void testFeetEquality_SameValue() {
 
+        MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
+
+        MeasurementApplication.Feet f2 = new MeasurementApplication.Feet(89.6);
+        assertEquals(f1, f2);
+
+    }
+    @Test
+    void testFeetEquality_DifferentValue() {
+    	MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
+    	MeasurementApplication.Feet f2 = new MeasurementApplication.Feet(34.6);
+        assertNotEquals(f1, f2);
+
+    }
+    @Test
+
+    void testFeetEquality_NullValue() {
+    	MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(5.0);
+        assertNotEquals(f1, null);
+
+    }
+
+    @Test
+    void testFeetEquality_DifferentType() {
+    	MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(5.0);
+        String other = "5.0";
+        assertNotEquals(f1, other);
+
+    }
 }
